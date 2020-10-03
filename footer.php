c134a88bf21ce035641ef8891d954c2ae3d87577
Rachel Seymour <rseymour@uw.edu>
2020-10-03 14:16:03 -0700

    Customized header and footer

--- footer.php
+++ footer.php
@@ -1,30 +1,18 @@
 <div role="contentinfo" class="uw-footer">
 
-<a href="http://www.washington.edu" class="footer-wordmark">University of Washington</a>
-
-<a href="http://www.washington.edu/boundless/"><h3 class="be-boundless">Be boundless</h3></a>
-
-<h4>Connect with us:</h4>
-
-<nav aria-label="social networking">
-    <ul class="footer-social">
-        <li><a class="facebook" href="http://www.facebook.com/UofWA">Facebook</a></li>
-        <li><a class="twitter" href="http://twitter.com/UW">Twitter</a></li>
-        <li><a class="instagram" href="http://instagram.com/uofwa">Instagram</a></li>
-        <li><a class="youtube" href="http://www.youtube.com/user/uwhuskies">YouTube</a></li>
-        <li><a class="linkedin" href="http://www.linkedin.com/company/university-of-washington">LinkedIn</a></li>
-        <li><a class="pinterest" href="http://www.pinterest.com/uofwa/">Pinterest</a></li>
+<nav aria-label="footer navigation">
+    <ul class="footer-links">
+        <li><a href="https://depts.washington.edu/hserv/">Department of Health Services</a></li>
+        <li><a href="https://sph.washington.edu/">School of Public Health</a></li>
     </ul>
 </nav>
 
+<a href="http://www.washington.edu" class="footer-wordmark">University of Washington</a>
+
 <nav aria-label="footer navigation">
     <ul class="footer-links">
-        <li><a href="http://www.uw.edu/accessibility">Accessibility</a></li>
-        <li><a href="http://uw.edu/contact">Contact Us</a></li>
-        <li><a href="http://www.washington.edu/jobs">Jobs</a></li>
-        <li><a href="http://www.washington.edu/safety">Campus Safety</a></li>
-        <li><a href="http://my.uw.edu/">My UW</a></li>
-        <li><a href="http://www.washington.edu/rules/wac">Rules Docket</a></li>
+        <li><a href="">Intranet</a></li>
+        <li><a href="">Contact Us</a></li>
         <li><a href="http://www.washington.edu/online/privacy/">Privacy</a></li>
         <li><a href="http://www.washington.edu/online/terms/">Terms</a></li>
     </ul>
